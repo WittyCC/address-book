bdf39e645a423fedb7691819de0e1112000d7fb2
Esvethlynna Pangelinan and Witty Chang <witty@>
2017-06-01 11:49:56 -0700

    Finalize html, styles, and scripts file.

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,25 @@ $(document).ready(function() {
     var contact3Input = $("input#contact3").val();
     var address3Input = $("input#address3").val();
 
+    $('.contact1').text(contact1Input);
+    $('.address1').text(address1Input);
+    $('.contact2').text(contact2Input);
+    $('.address2').text(address2Input);
+    $('.contact3').text(contact3Input);
+    $('.address3').text(address3Input);
+
     $("#contacts").show();
     $("#blanks").hide();
+
+    $('.contact1').click(function() {
+      $('.address1').show();
+    });
+    $('.contact2').click(function() {
+      $('.address2').show();
+    });
+    $('.contact3').click(function() {
+      $('.address3').show();
+    });
+
   });
 });
